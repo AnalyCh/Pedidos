7fa6f7b39411cb0020f3eced0bb07ae49ec5f860
MichaelDrake <41341651+MichaelDrake@users.noreply.github.com>
2018-11-24 02:05:53 -0500

    Script ejecutable

--- AGRANDAR LONGITUD DE CODCLI.sql
+++ AGRANDAR LONGITUD DE CODCLI.sql
@@ -1,16 +1,19 @@
 use PedidosPruebas
 go
-sp_help 'catalogo.cliente'
-go
+
 --/////////////////////////////////////////////////////////////////////////////////////////////
 ------------ELIMINAR RESTRICCIONES DE LA TABLA DEL CLIENTE------------------------------------
 alter table catalogo.cliente drop constraint debeTener
+go
 alter table catalogo.cliente drop constraint ubicado
+go
 alter table catalogo.cliente drop constraint codcli
+go
 
 --/////////////////////////////////////////////////////////////////////////////////////////////
 -------------ELIMINAR RESTRICCIONES DE TABLAS DONDE ES FORANEA----------------------------------
 alter table movimiento.cabecerap drop constraint solicita
+go
 ------------------------------------------------------------------------------------------------
 ------------------------------------------------------------------------------------------------
 
@@ -42,57 +45,99 @@ go
 --actualizar los datos segun la longitud de la socilitud de cambio
 -----------CODCLI----------------------------------------------
 update catalogo.cliente set codcli = 'C00001' where codCli='c01'
+go
 update catalogo.cliente set codcli = 'C00002' where codCli='c02'
+go
 update catalogo.cliente set codcli = 'C00003' where codCli='c03'
+go
 update catalogo.cliente set codcli = 'C00004' where codCli='c04'
+go
 update catalogo.cliente set codcli = 'C00005' where codCli='c05'
+go
 update catalogo.cliente set codcli = 'C00006' where codCli='c06'
+go
 update catalogo.cliente set codcli = 'C00007' where codCli='c07'
+go
 
 ---------------GARANTE-------------------------------------------
 update catalogo.cliente set garante = 'C00001' where garante='c01'
+go
 update catalogo.cliente set garante = 'C00002' where garante='c02'
+go
 update catalogo.cliente set garante = 'C00003' where garante='c03'
+go
 update catalogo.cliente set garante = 'C00004' where garante='c04'
+go
 update catalogo.cliente set garante = 'C00005' where garante='c05'
+go
 update catalogo.cliente set garante= 'C00006' where  garante='c06'
+go
 update catalogo.cliente set garante = 'C00007' where garante='c07'
+go
 
 ----------------CODLI(TABLA:CABECERAP)----------------------------
 update movimiento.cabecerap set codcli = 'C00001' where codcli='c01'
+go
 update movimiento.cabecerap set codcli = 'C00002' where codcli='c02'
+go
 update movimiento.cabecerap set codcli = 'C00003' where codcli='c03'
+go
 update movimiento.cabecerap set codcli = 'C00004' where codcli='c04'
+go
 update movimiento.cabecerap set codcli = 'C00005' where codcli='c05'
+go
 update movimiento.cabecerap set codcli = 'C00006' where codcli='c06'
+go
 update movimiento.cabecerap set codcli = 'C00007' where codcli='c07'
+go
 -----------------codcli(TABLA: CABECERADETALLEP)-------------------
-sp_help 'movimiento.CABECERADETALLEP'
+
 update movimiento.CABECERADETALLEP set codcli = 'C00001' where codcli='c01'
+go
 update movimiento.CABECERADETALLEP set codcli = 'C00002' where codcli='c02'
+go
 update movimiento.CABECERADETALLEP set codcli = 'C00003' where codcli='c03'
+go
 update movimiento.CABECERADETALLEP set codcli = 'C00004' where codcli='c04'
+go
 update movimiento.CABECERADETALLEP set codcli = 'C00005' where codcli='c05'
+go
 update movimiento.CABECERADETALLEP set codcli = 'C00006' where codcli='c06'
+go
 update movimiento.CABECERADETALLEP set codcli = 'C00007' where codcli='c07'
+go
 --------------------PAGOS------------------------------------------------
 sp_help 'movimiento.pagos'
 update MOVIMIENTO.PAGOS set codcli = 'C00001' where codcli='c01'
+go
 update MOVIMIENTO.PAGOS set codcli = 'C00002' where codcli='c02'
+go
 update MOVIMIENTO.PAGOS set codcli = 'C00003' where codcli='c03'
+go
 update MOVIMIENTO.PAGOS set codcli = 'C00004' where codcli='c04'
+go
 update MOVIMIENTO.PAGOS set codcli = 'C00005' where codcli='c05'
+go
 update MOVIMIENTO.PAGOS set codcli = 'C00006' where codcli='c06'
+go
 update MOVIMIENTO.PAGOS set codcli = 'C00007' where codcli='c07'
+go
 -------------------DEUDOR-----------------------------------------------------
-sp_help 'catalogo.deudor'
+
 update catalogo.DEUDOR set codcli = 'C00001' where codcli='c01'
+go
 update catalogo.DEUDOR set codcli = 'C00002' where codcli='c02'
+go
 update catalogo.DEUDOR set codcli = 'C00003' where codcli='c03'
+go
 update catalogo.DEUDOR set codcli = 'C00004' where codcli='c04'
+go
 update catalogo.DEUDOR set codcli = 'C00005' where codcli='c05'
+go
 update catalogo.DEUDOR set codcli = 'C00006' where codcli='c06'
+go
 update catalogo.DEUDOR set codcli = 'C00007' where codcli='c07'
+go
 
 
 --//////////////////////////////////////////////////////////////////////////////////////////////////
